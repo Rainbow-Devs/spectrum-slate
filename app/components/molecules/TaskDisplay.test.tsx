0758cb87c6403896e6ebd1150efa982c7cd287d5
Michael Trew <mtrew2015@gmail.com>
2023-09-26 22:22:58 -0400

    spectrum-37: switch to jest

---
+++ app/components/molecules/TaskDisplay.test.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import TaskDisplay from "./TaskDisplay";
+import type { Task } from "@prisma/client";
+import "@testing-library/jest-dom";
+
+const task = {
+  id: 1,
+  title: "Test Task",
+  status: "In Progress",
+  dueDate: new Date("01-01-2022"),
+} as unknown as Task;
+
+describe("TaskDisplay", () => {
+  it("renders the task title", () => {
+    render(<TaskDisplay task={task} />);
+    expect(screen.getByText("Test Task")).toBeInTheDocument();
+  });
+
+  it("renders the task status", () => {
+    render(<TaskDisplay task={task} />);
+    expect(screen.getByText("Status: In Progress")).toBeInTheDocument();
+  });
+
+  it("renders the task due date", () => {
+    render(<TaskDisplay task={task} />);
+    expect(screen.getByText("Due: Jan 1, 2022")).toBeInTheDocument();
+  });
+
+  it("does not render the task due date if it is null", () => {
+    const taskWithoutDueDate = { ...task, dueDate: null };
+    render(<TaskDisplay task={taskWithoutDueDate} />);
+    expect(screen.queryByText("Due:")).not.toBeInTheDocument();
+  });
+});

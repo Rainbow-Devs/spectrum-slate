4ec695d8fe721c3f82f8692b9578a8b56352f63b
Michael Trew <mtrew2015@gmail.com>
2023-09-27 23:24:37 -0400

    spectrum-33: add context menu to each task

--- app/components/molecules/TaskDisplay.tsx
+++ app/components/molecules/TaskDisplay.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import type { Task } from "@prisma/client";
 import { Card } from "../ui/card";
 import { StatusDot } from "../atoms/StatusDot";
-
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "../atoms/context-menu";
 interface TaskProps {
   task: Task;
 }
@@ -20,13 +25,26 @@ export default function TaskDisplay({ task }: TaskProps) {
   };
   const dueDate = formatDate(task?.dueDate);
   return (
-    <Card className="w-[75%] md:w-[30%] m-2 p-2 flex items-center">
-      <StatusDot status={task.status} />
-      <div className="ml-3 text-m w-[100%]">
-        <h2 className="text-m"><span className="font-bold">Title:</span>{task.title}</h2>
-        <p className="text-sm">Status: {task.status}</p>
-        {dueDate && <p className="text-sm text-slate-500">Due: {dueDate}</p>}
-      </div>
-    </Card>
+    <ContextMenu>
+      <ContextMenuTrigger className="flex flex-wrap items-center w-[30%]">
+        <Card className="w-[100%] m-2 p-2 flex items-center">
+          <StatusDot status={task.status} />
+          <div className="ml-3 text-m w-[100%]">
+            <h2 className="text-m">
+              <span className="font-bold">Title:</span>
+              {task.title}
+            </h2>
+            <p className="text-sm">Status: {task.status}</p>
+            {dueDate && (
+              <p className="text-sm text-slate-500">Due: {dueDate}</p>
+            )}
+          </div>
+        </Card>
+      </ContextMenuTrigger>
+      <ContextMenuContent className="bg-slate-500 text-white">
+        <ContextMenuItem>Edit</ContextMenuItem>
+        <ContextMenuItem>Delete</ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 }
